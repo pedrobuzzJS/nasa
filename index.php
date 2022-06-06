239b5f1d84a6efcb4ecc0d084a3a641825ac77f6
Pedro Artur Buzzi Pereira <pedro.buzzi@unidavi.edu.br>
2022-06-06 03:44:41 -0300

    Adicionado manifesto e service-worker

--- index.php
+++ index.php
@@ -1,21 +1,52 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="/service-worker.js"></script>
+		<link rel="manifest" href="/manifest.json" crossorigin="use-credentials">
+
     <title>Projeto Nasa</title>
     <meta charset="utf-8"/>
     <link href="./css/style.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </head>
-  <body> 
-      <header class="header">
-          <label for="btn">&#9776;</label>
-          <a href="./index.php" class="menu-logo">Projeto Nasa</a>
-          <nav>
-            <ul class="menu">
-              <li><a href="./index.php">Inicio</a></li>
-              <li><a href="./foguete.php">Foguete</a></li>
-          </ul>
-          </nav>
-      </header>
+  <body>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="./index.php">NASA</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="./index.php">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./foguete.php">Foguete</a>
+              </li>            
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <input type="checkbox" id="btn">
       <div class="menu11 ">
         <nav>
@@ -93,6 +124,18 @@
          <div class="footer-bottom">
            &copy; Projeto Nasa | Designed by Pedro Buzzi
          </div>
-       </footer>      
+       </footer>  
+       
+      <script>
+        if ('serviceWorker' in navigator) {
+	     		navigator.serviceWorker.register('/service-worker.js')
+	     		.then(function(registration) {
+	     			console.log('Registration successful, scope is:', registration.scope);
+	     		})
+	     		.catch(function(error) {
+	     			console.log('Service worker registration failed, error:', error);
+	     		});
+	    	}
+      </script>
   </body>
 </html>
